af639d2aa368c44ab679a5d24c460ce3c4000493
Taisuke Fukuno <fukuno@jig.jp>
2026-03-02 08:36:26 +0900

    move embed to another repo
    https://github.com/code4fukui/embed

--- getEmbed.example.js
+++
@@ -1,5 +0,0 @@
-import { getEmbed } from "./getEmbed.js";
-
-const s = "あいうえ";
-const res = await getEmbed(s);
-console.log(res);
--- getsimilar.js
+++
@@ -1,7 +0,0 @@
-import { Embed } from "./Embed.js";
-
-const embed = await Embed.create("./data.csv");
-
-const q = Deno.args[0];
-const top = await embed.getSimilar(q);
-console.log(top);

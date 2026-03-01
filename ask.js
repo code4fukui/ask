6df5d6f3f6ff0198db21d6e8f1cc2def25da07cc
Taisuke Fukuno <fukuno@jig.jp>
2026-03-01 23:19:23 +0900

    separate showChatStreaming.js

--- ask.js
+++ ask.js
@@ -1,38 +1,7 @@
-import ollama from "npm:ollama";
+import { showChatStreaming } from "./showChatStreaming.js";
 
 const model = Deno.env.get("AI_MODEL");
-if (!model) {
-  throw new Error("set AI_MODEL");
-}
 
 const q = Deno.args[0];
 
-const stream = await ollama.chat({
-  model,
-  messages: [{ role: "user", content: q }],
-  stream: true,
-})
-
-let inThinking = false
-let content = ""
-let thinking = ""
-
-for await (const chunk of stream) {
-  if (chunk.message.thinking) {
-    if (!inThinking) {
-      inThinking = true
-      process.stdout.write("Thinking:\n")
-    }
-    process.stdout.write(chunk.message.thinking)
-    // accumulate the partial thinking
-    thinking += chunk.message.thinking
-  } else if (chunk.message.content) {
-    if (inThinking) {
-      inThinking = false
-      process.stdout.write("\n\nAnswer:\n")
-    }
-    process.stdout.write(chunk.message.content)
-    // accumulate the partial content
-    content += chunk.message.content
-  }
-}
+await showChatStreaming(q, model);

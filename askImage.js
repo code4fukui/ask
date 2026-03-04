04708000863c71f71700c3ec00d1b71a0b8236d9
Taisuke Fukuno <fukuno@jig.jp>
2026-03-04 19:57:39 +0900

    change the interface of askImage

--- askImage.js
+++ askImage.js
@@ -1,18 +1,13 @@
 import { Base64 } from "https://code4fukui.github.io/Base64/Base64.js";
 
-export const askImage = async (jpeg, model = "gemma3:4b") => {
-  const b64 = Base64.encode(jpeg);
+export const askImage = async (img, prompt, model = "gemma3:4b") => {
+  const b64 = Base64.encode(img);
 
   const body = {
     model,
     stream: false,
     format: "json",
-    prompt: [
-      "この画像に写っているものを、日本語のキーワードと、写っている人数、動物の数だけで返してください。",
-      "出力は必ず JSON で、次の形式にしてください：",
-      '{ "keywords": ["...","...","..."] }, "human_count": n, "animal_count": m ',
-      "keywords は重複なし、できれば 5〜12 個。固有名詞より一般名詞を優先。"
-    ].join("\n"),
+    prompt,
     images: [b64],
   };
 
--- askimg.js
+++ askimg.js
@@ -3,6 +3,11 @@ import { askImage } from "./askImage.js";
 const model = Deno.env.get("AI_MODEL");
 const fn = Deno.args[0];
 
+const prompt = `この画像に写っているものを、日本語のキーワードと、写っている人数、動物の数だけで返してください。
+出力は必ず JSON で、次の形式にしてください：
+{ "keywords": ["...","...","..."] }, "human_count": n, "animal_count": m }
+"keywords は重複なし、できれば 5〜12 個。固有名詞より一般名詞を優先。`
+
 const bin = await Deno.readFile(fn);
-const res = await askImage(bin, model);
+const res = await askImage(bin, prompt, model);
 console.log(res);
